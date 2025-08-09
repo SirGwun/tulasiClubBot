08b91590fa046b510c2a0d7d027b3d99e2ca27c6
Radharaman <z2a3q4@gmail.com>
2025-08-10 01:57:01 +0400

    Тестовый комит, засквошить

--- src/main/java/bot/core/Main.java
+++ src/main/java/bot/core/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static boolean test;
 
     public static void main(String[] args) {
+
         for (String arg : args) {
             if (arg.equals("--test")) {
                 test = true;
