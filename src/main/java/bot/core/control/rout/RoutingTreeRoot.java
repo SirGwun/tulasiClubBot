fd6b643a920e03aa63d29607cc08b23018398a7c
Radharaman <z2a3q4@gmail.com>
2026-01-31 00:45:54 +0400

    Add point of entering into Routing tree

---
+++ src/main/java/bot/core/control/rout/RoutingTreeRoot.java
@@ -0,0 +1,18 @@
+package bot.core.control.rout;
+
+import bot.core.control.rout.classify.enums.InputSources;
+import bot.core.model.input.Input;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoutingTreeRoot {
+    Classifier<Input, InputSources> root;
+
+    public RoutingTreeRoot(Classifier<Input, InputSources> root) {
+        this.root = root;
+    }
+
+    InputHandler<Input> getRoutingRoot() { // check illegal argument ex
+        return root;
+    }
+}
