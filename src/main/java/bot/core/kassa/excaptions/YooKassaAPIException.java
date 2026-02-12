f199ee2754f8c8902623d3983b45944068c1a04b
Radharaman <z2a3q4@gmail.com>
2026-02-12 18:09:13 +0400

    Add http status code in ApiException

--- src/main/java/bot/core/kassa/excaptions/YooKassaAPIException.java
+++ src/main/java/bot/core/kassa/excaptions/YooKassaAPIException.java
@@ -1,10 +1,17 @@
 package bot.core.kassa.excaptions;
 
 public class YooKassaAPIException extends RuntimeException {
+
+
+
     int status;
 
     public YooKassaAPIException(String message, int status) {
         super(message);
         this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
     }
 }
